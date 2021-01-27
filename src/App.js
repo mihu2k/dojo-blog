b10ade4ea551b414c71a9f0aa382291a59a07ec8
mihu2k <hungt2766@gmail.com>
2021-01-27 18:27:41 +0700

    adding styles

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
-import './App.css';
+import Navbar from './components/Narbar';
+import Home from './components/Home';
 
 function App() {
+    const title = 'Welcome to my blog';
+    const link = 'https://www.google.com/';
+
     return (
         <div className="App">
+            <Navbar />
             <div className="content">
-                <h1>App Component</h1>
+                <h1>{ title }</h1>
+                <Home />
             </div>
+
+            <a href={ link }>Google</a>
         </div>
     );
 }
